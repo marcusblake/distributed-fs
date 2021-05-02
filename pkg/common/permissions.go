f58c403f652b8d883393c0b2755c35f122bfbebe
Marcus  Blake <blakemarcus15@gmail.com>
2021-05-02 15:58:10 -0400

    Modify notion of permission and separate operations definition from permissions definition

---
+++ pkg/common/permissions.go
@@ -0,0 +1,29 @@
+package common
+
+// PermissionType indicates the actions allowed on a file
+type PermissionType uint8
+
+// PermissionGroup indicates the file permission group
+type PermissionGroup uint8
+
+// Defines the permissions for a particular group
+var GroupPermissions = struct {
+	Application      PermissionGroup
+	ApplicationGroup PermissionGroup
+	All              PermissionGroup
+}{
+	Application:      1,
+	ApplicationGroup: 1 << 1,
+	All:              1 << 2,
+}
+
+// Defines permissions that applications have for a file
+var Permission = struct {
+	Read   PermissionType
+	Write  PermissionType
+	Delete PermissionType
+}{
+	Read:   1,
+	Write:  1 << 1,
+	Delete: 1 << 2,
+}
